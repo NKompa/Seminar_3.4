a442ae18590e22e8d441e055d34d2ac4acbb23f1
NKompa <nkompa7@gmail.com>
2022-08-29 20:43:27 +0300

    Sum up to number

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма = "+GetSum(num));
+
+int GetSum (int number)
+{
+    int sum = 0;
+    int count = 0;
+
+    while (number>count)
+    {
+        count++;
+        sum+=count;
+    }
+    return sum;
+}
