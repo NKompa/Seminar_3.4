5203dedab0db50186321df2109174ecb303710e5
NKompa <nkompa7@gmail.com>
2022-09-08 14:47:27 +0300

    Fill array with random  0 & 1

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,27 +1,41 @@
 ﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке. [1,0,1,1,0,1,0,0]
 
-int [] array = new int [8];
+// int [] array = new int [8];
 
-void FillArray(int[]collection)
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(0,2);
-        index++;
-    }
-}
-void PrintArray(int[]col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.Write(col[position]+" ");
-        position++;
-    }
-}
+// void FillArray(int[]collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(0,2);
+//         index++;
+//     }
+// }
+// void PrintArray(int[]col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write(col[position]+" ");
+//         position++;
+//     }
+// }
 
-FillArray(array);
-PrintArray(array);
+// FillArray(array);
+// PrintArray(array);
+
+int[] array = GetArray(8);
+Console.WriteLine(string.Join(",", array));
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+
+}
